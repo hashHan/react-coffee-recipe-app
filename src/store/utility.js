b7090a62f133fa3aa9c8b0372c86b31c85b170f9
hashHan <haeseong.han@gmail.com>
2018-04-09 21:27:26 +0900

    shared folder, lazy routing

--- src/store/utility.js
+++
@@ -1,8 +0,0 @@
-//destructuring and update to avoid unpredictable things
-
-export const updateObject = (oldObject, updatedProperties) => {
-    return {
-        ...oldObject,
-        ...updatedProperties
-    };
-};
